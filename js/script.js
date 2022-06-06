2ce53d8c62ffa0a134e6a5fbbddf511f1bdecb28
Tanzir Rifat <93050315+tanzirrahmanrifat@users.noreply.github.com>
2022-06-06 22:03:12 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,3 +5,32 @@ window.onload = ()=>{
     $("#num3").countMe(40, 50);
  }
 
+// head start
+
+
+  //  scrolltop
+$(document).ready(function () {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 600) {
+        $("#topbtn").fadeIn();
+      } else {
+        $("#topbtn").fadeOut();
+      }
+    });
+  
+    $("#topbtn").click(function () {
+      $("html ,body").animate(
+        {
+          scrollTop: 0,
+        },
+        800
+      );
+    });
+  });
+  
+  // preloader
+  
+  let loader = document.getElementById("preloader");
+  window.addEventListener("load", function () {
+    loader.style.display = "none";
+  });
